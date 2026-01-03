b2df76590f4508a3769c07fd7780e514e6352ccc
Mathis003 <mathis.delsart@student.uclouvain.be>
2026-01-03 20:10:48 +0100

    Modify build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/ProfPage",
+  assetPrefix: "/ProfPage/",
 };
 
 module.exports = nextConfig;
