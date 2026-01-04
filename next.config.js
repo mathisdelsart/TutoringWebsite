c31dd2fe16de268ba53daffdba7c4faefdbffb92
Mathis003 <mathis.delsart@student.uclouvain.be>
2026-01-04 01:06:12 +0100

    Fix basePath for GitHub Pages deployment (Remove Commit)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/PageProf" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/PageProf" : "",
   images: {
     unoptimized: true,
   },
